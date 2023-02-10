0ac7fd8a7143750bba7aedc26da5cd95b4a89239
an-ivannikov <info@ivannikov.pro>
2023-02-10 21:18:34 +0400

    Update CashRushNft.sol

--- contracts/CashRushNft.sol
+++ contracts/CashRushNft.sol
@@ -1,6 +1,142 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-contract CashRushNft {
-    //
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract CashRushNft is
+    ERC721,
+    ERC721Enumerable,
+    ERC721Burnable,
+    Ownable,
+    EIP712,
+    ERC721Votes
+{
+    using Counters for Counters.Counter;
+
+    uint256 private constant MAX_SUPPLY = 5000;
+    Counters.Counter private _tokenIdCounter;
+
+    // Metadata
+    string private _name = "CashRush";
+    string private _symbol = "CASHRUSH";
+    string private _contractURI =
+        "https://ipfs.cashrush.gg/ipfs/?/contract.json";
+    string private _baseURL = "https://ipfs.cashrush.gg/ipfs/?/";
+    string private _baseExtension = ".json";
+    bool private _revealed = true;
+    string private _notRevealedURI =
+        "https://ipfs.cashrush.gg/ipfs/?/notrevealed.json";
+
+    constructor() ERC721(_name, _symbol) EIP712(_name, "1") {
+        _tokenIdCounter.increment(); // set to 1
+    }
+
+    // CashRush
+
+    // Mint
+    function safeMint(address to, uint256 tokenCount) external onlyOwner {
+        require((totalSupply() + tokenCount) <= MAX_SUPPLY, "MAX_SUPPLY");
+        for (uint256 i = 0; i < tokenCount; i++) {
+            uint256 tokenId = _tokenIdCounter.current();
+            _tokenIdCounter.increment();
+            _safeMint(to, tokenId);
+        }
+    }
+
+    // Extra
+    function rawOwnerOf(uint256 tokenId) external view returns (address) {
+        return _ownerOf(tokenId);
+    }
+
+    function tokensOfOwner(address owner)
+        external
+        view
+        returns (uint256[] memory)
+    {
+        uint256 tokenCount = balanceOf(owner);
+        require(0 < tokenCount, "ERC721Enumerable: owner index out of bounds");
+        uint256[] memory tokenIds = new uint256[](tokenCount);
+        for (uint256 i = 0; i < tokenCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(owner, i);
+        }
+        return tokenIds;
+    }
+
+    // Metadata
+    function reveal() external onlyOwner {
+        _revealed = true;
+    }
+
+    function setNotRevealedURI(string memory uri_) external onlyOwner {
+        _notRevealedURI = uri_;
+    }
+
+    function contractURI() external view returns (string memory) {
+        return _contractURI;
+    }
+
+    function setContractURI(string memory uri_) external onlyOwner {
+        _contractURI = uri_;
+    }
+
+    function tokenURI(uint256 tokenId)
+        external
+        view
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        if (!_revealed) return _notRevealedURI;
+
+        return
+            string(
+                abi.encodePacked(_baseURL, tokenId.toString(), _baseExtension)
+            );
+    }
+
+    function setBaseURI(string memory uri_) external onlyOwner {
+        _baseURL = uri_;
+    }
+
+    function setBaseExtension(string memory fileExtension) external onlyOwner {
+        _baseExtension = fileExtension;
+    }
+
+    // Override
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize
+    ) internal override(ERC721, ERC721Votes) {
+        super._afterTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
