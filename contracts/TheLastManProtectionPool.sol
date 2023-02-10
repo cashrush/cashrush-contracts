942c24a3b457513e1fffd5cafb75cdc33c4e409a
an-ivannikov <info@ivannikov.pro>
2023-02-10 19:44:33 +0400

    Create TheLastManProtectionPool.sol

---
+++ contracts/TheLastManProtectionPool.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+contract TheLastManProtectionPool {
+    uint256 public constant DELAY = 5 days;
+    uint256 public constant SHARES = 50;
+    uint256 public lastDeposit = 0;
+    uint256 public nextId = 0;
+    mapping(uint256 => address) public accounts;
+    mapping(address => bool) public depositors;
+
+    event Received(address indexed account, uint256 value);
+    event Deposited(address indexed account, uint256 id, uint256 value);
+    event Distributed(address indexed account, uint256 id, uint256 value);
+
+    constructor(address[] memory _depositors) public {
+        for (uint256 i = 0; i < _depositors.length; i++) {
+            depositors[_depositors[i]] = true;
+        }
+    }
+
+    modifier onlyDepositors() {
+        require(depositors[msg.sender], "Not allowed");
+        _;
+    }
+
+    function distribute() external {
+        if (block.timestamp >= (lastDeposit + DELAY)) {
+            uint256 shares = _min(SHARES, nextId);
+            uint256 value = address(this).balance / shares;
+            for (uint256 i = 1; i <= shares; i++) {
+                uint256 id = nextId - i;
+                address payable recipient = payable(accounts[id]);
+                recipient.transfer(value);
+                emit Distributed(recipient, id, value);
+            }
+        }
+    }
+
+    function deposit(address account) external payable onlyDepositors {
+        lastDeposit = block.timestamp;
+        uint256 id = nextId++;
+        accounts[id] = account;
+        emit Deposited(account, id, msg.value);
+    }
+
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
+    }
+
+    function _min(uint256 a, uint256 b) private pure returns (uint256) {
+        if (a < b) return a;
+        else return b;
+    }
+}
