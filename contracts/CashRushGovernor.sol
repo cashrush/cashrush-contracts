75e5777c5fb2bfe6e2739c7916d0ee84f35724f2
an-ivannikov <info@ivannikov.pro>
2023-03-22 15:11:33 +0400

    Update CashRushGovernor.sol

--- contracts/CashRushGovernor.sol
+++ contracts/CashRushGovernor.sol
@@ -1,18 +1,16 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/governance/Governor.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
-import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
 
 contract CashRushGovernor is
     Governor,
     GovernorSettings,
     GovernorCountingSimple,
-    GovernorVotes,
-    GovernorVotesQuorumFraction
+    GovernorVotes
 {
     constructor(
         IVotes _token
@@ -20,14 +18,19 @@ contract CashRushGovernor is
         Governor("CashRushGovernor")
         GovernorSettings(0, 50400, 1)
         GovernorVotes(_token)
-        GovernorVotesQuorumFraction(1)
     {
         /**
-         * Voting Delay - 1 block
+         * Voting Delay - 0 blocks
          * Voting Period - 1 week = 7*24*60*60/12 = 50400 blocks
-         * Proposal Threshold - 1
-         * Quorum % - 1%
+         * Proposal Threshold # - 1 NFTs
+         * Quorum # - 50 NFTs
          */
+    }
+
+    function quorum(
+        uint256 blockNumber
+    ) public pure override returns (uint256) {
+        return 50;
     }
 
     // The following functions are overrides required by Solidity.
@@ -47,17 +50,6 @@ contract CashRushGovernor is
         returns (uint256)
     {
         return super.votingPeriod();
-    }
-
-    function quorum(
-        uint256 blockNumber
-    )
-        public
-        view
-        override(IGovernor, GovernorVotesQuorumFraction)
-        returns (uint256)
-    {
-        return super.quorum(blockNumber);
     }
 
     function proposalThreshold()
